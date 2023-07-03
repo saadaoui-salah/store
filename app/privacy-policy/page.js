ee4a2ce8615f05e045eb8d17210184dd259df7e0
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-07-03 19:43:30 -0400

    :heavy_plus_sign: add privacy policy page

---
+++ app/privacy-policy/page.js
@@ -0,0 +1,106 @@
+export default function PrivacyPolicy() {
+  return (
+    <div className="text-black px-16 text-xl my-16">
+      <h1 className="text-4xl mb-4">Privacy Policy</h1>
+      <p className="mb-2">
+        At Comercify, we are dedicated to safeguarding your privacy and
+        protecting your personal information. This Privacy Policy outlines how
+        we collect, use, disclose, and protect the data we gather from our
+        website visitors and customers. By accessing or using our website and
+        services, you consent to the practices described in this policy.
+      </p>
+      <div>
+        <h2 className="font-bold mb-2">1- Information We Collect:</h2>
+        <p className="mb-2 px-2">
+          We may collect various types of information when you interact with our
+          website, such as your name, email address, contact details, browsing
+          history, and demographic information. This information is voluntarily
+          provided by you or automatically collected through cookies, web
+          beacons, and other tracking technologies. We use this information to
+          personalize your experience, improve our services, and communicate
+          with you effectively.
+        </p>
+      </div>
+      <h2 className="font-bold mb-2">2- Use of Information:</h2>
+      <p className="mb-2 px-2 font-bold">
+        We may use the collected information to:
+      </p>
+      <p className="mb-2 px-6">
+        Provide and improve our products, services, and customer support.
+      </p>
+      <p className="mb-2 px-6">
+        Customize your experience and personalize the content you see.
+      </p>
+      <p className="mb-2 px-6">
+        Communicate with you regarding updates, offers, and promotions.
+      </p>
+      <p className="mb-2 px-6">
+        Conduct research and analysis to enhance our website and services.
+      </p>
+      <p className="mb-2 px-6">
+        Comply with legal obligations and enforce our terms and policies.
+      </p>
+      <h2 className="font-bold mb-2 mt-6">1- Sharing of Information:</h2>
+      <p className="mb-2 px-2">
+        We may share your information with trusted third parties who assist us
+        in operating our business, providing services, or conducting activities
+        on our behalf. These parties are obligated to maintain the
+        confidentiality and security of your information. We may also share your
+        information when required by law, to protect our rights, or in the event
+        of a corporate merger, acquisition, or sale.
+      </p>
+      <h2 className="font-bold mb-2">2- Cookies and Tracking Technologies:</h2>
+      <p className="mb-2 px-2">
+        Our website uses cookies, web beacons, and similar tracking technologies
+        to collect information about your browsing activities. These
+        technologies help us analyze trends, administer the site, track users'
+        movements, and gather demographic information. You have the option to
+        manage your cookie preferences through your browser settings, but please
+        note that disabling cookies may impact your experience on our website.
+      </p>
+      <h2 className="font-bold mb-2">3- Data Security:</h2>
+      <p className="mb-2 px-2">
+        We employ industry-standard security measures to protect your
+        information against unauthorized access, alteration, disclosure, or
+        destruction. However, please be aware that no method of transmission or
+        storage can guarantee absolute security. We cannot be held responsible
+        for any unauthorized access or breaches that are beyond our control.
+      </p>
+      <h2 className="font-bold mb-2">4- Third-Party Links:</h2>
+      <p className="mb-2 px-2">
+        Our website may contain links to third-party websites or services.
+        Please note that we are not responsible for the privacy practices or
+        content of these external sites. We encourage you to review the privacy
+        policies of any third-party websites before providing any personal
+        information.
+      </p>
+      {/* <h2 className="font-bold mb-2">Children's Privacy:</h2>
+      <p className="mb-2 px-2">
+        Our website and services are not intended for individuals under the age
+        of [minimum age]. We do not knowingly collect or solicit personal
+        information from children. If we become aware that we have inadvertently
+        collected personal information from a child, we will take immediate
+        steps to delete it from our servers.
+      </p> */}
+      <h2 className="font-bold mb-2">6- Changes to the Privacy Policy:</h2>
+      <p className="mb-2 px-2">
+        We may update this Privacy Policy from time to time to reflect changes
+        in our practices or legal requirements. We encourage you to periodically
+        review this page for any updates. Your continued use of our website and
+        services after the changes will signify your acceptance of the revised
+        policy.
+      </p>
+      <p className="mb-2 px-2">
+        If you have any questions, concerns, or requests regarding our Privacy
+        Policy or the handling of your personal information, please contact us
+        using the information provided in the "Contact Us" section on our
+        website.
+      </p>
+      <p className="mb-2 px-2">
+        Thank you for taking the time to read our Privacy Policy. Your privacy
+        matters to us, and we are committed to maintaining your trust and
+        confidence in our services.
+      </p>
+    </div>
+  );
+}
