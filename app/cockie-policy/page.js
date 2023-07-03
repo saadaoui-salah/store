4ac6b8beca385e561cffe5b9861e78e75407f023
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-07-03 19:42:14 -0400

    :heavy_plus_sign: add cockies policy page

---
+++ app/cockie-policy/page.js
@@ -0,0 +1,46 @@
+export default function Privacy() {
+  return (
+    <div className="w-full pt-16 h-full flex items-center justify-center">
+      <div className="px-16 mb-8">
+        <h1 className="font-bold text-black text-4xl mb-12">Cookie Policy</h1>
+        <p className="mt-8 text-black text-xl font-bold">
+          Welcome to our website!
+        </p>
+        <p className="mt-8 text-black text-xl">
+          We would like to inform you that we use cookies, which are small text
+          files stored on your device when you visit our website. These cookies
+          play an important role in enhancing your browsing experience by
+          remembering your preferences, analyzing website traffic, and tailoring
+          advertisements to align with your interests. Additionally, cookies
+          enable various functionalities on our site, such as facilitating
+          seamless logins to your account and enabling the convenient addition
+          of items to your shopping cart.
+        </p>
+        <p className="mt-8 text-black text-xl">
+          To provide you with greater control and transparency, we offer you the
+          opportunity to manage your cookie preferences. By clicking on "Cookie
+          Settings," you can customize your cookie choices, allowing you to
+          select the specific types of cookies you wish to accept or decline.
+          However, it's essential to note that certain functionalities of our
+          website may be affected if you choose to block or disable certain
+          cookies.
+        </p>
+        <p className="mt-8 text-black text-xl">
+          At our company, we prioritize your privacy and are committed to
+          handling your personal information securely. We encourage you to
+          review our comprehensive Privacy Policy, which outlines how we
+          collect, use, and protect your data.
+        </p>
+        <p className="mt-8 text-black text-xl">
+          We sincerely appreciate your visit to our website, and we hope you
+          have a delightful browsing experience with us. If you have any further
+          questions or concerns regarding cookies or our privacy practices,
+          please do not hesitate to reach out to our dedicated support team.
+        </p>
+        <p className="mt-8 text-black text-xl">
+          Thank you once again for choosing our website.
+        </p>
+      </div>
+    </div>
+  );
+}
