911dcb874b0bba3b9a34d7fa058fb6e897a92eb8
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-07-03 18:37:23 -0400

    :heavy_plus_sign: add open & close icons

---
+++ comps/shared/Icons.js
@@ -0,0 +1,39 @@
+export const CloseMenuIcon = () => {
+  return (
+    <svg
+      className="block h-6 w-6"
+      id="close"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      aria-hidden="true"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+      />
+    </svg>
+  );
+};
+
+export const OpenMenuIcon = () => {
+  return (
+    <svg
+      id="open"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      aria-hidden="true"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18L18 6M6 6l12 12"
+      />
+    </svg>
+  );
+};
