b65a98fd260a51117277bbbe7d09be0ed60d4d78
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-07-03 18:35:00 -0400

    ```init```

---
+++ comps/index.js
@@ -0,0 +1,5 @@
+export { default as Nav } from "./Nav";
+export { default as Footer } from "./Footer";
+export { default as AppBanner } from "./AppBanner";
+export { default as Banner } from "./Banner";
+export { default as ProductCard } from "./ProductCard";
