be7cf494257adf6afeb9ddebf48861eaaaab1e4a
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-07-03 18:33:32 -0400

    :heavy_plus_sign: add banner

---
+++ comps/Banner.js
@@ -0,0 +1,14 @@
+const Banner = () => {
+  return (
+    <div className="w-full h-[400px] bg-gray-700 flex flex-col items-center justify-center">
+      <button className="text-white hover:bg-blue-700 font-bold text-lg bg-blue-600 px-6 py-4 rounded-md">
+        Shop Now
+      </button>
+      <p className="text-gray-100 mt-4 font-semibold">
+        Get super watchs with less price
+      </p>
+    </div>
+  );
+};
+
+export default Banner;
