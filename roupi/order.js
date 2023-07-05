ff5a5ac79261488a4b13a1d05bb4c513c69d41f2
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-07-04 23:07:16 -0400

    :heavy_plus_sign: add order validation

--- roupi/order.js
+++ roupi/order.js
@@ -2,16 +2,58 @@
 
 import { useStateContext } from "@/context/contextProvider";
 import { usePOST } from "./utils";
+import { useState } from "react";
 
 export const useCreateOrder = () => {
-  const { handleNotification, token, trackID } = useStateContext();
-  const submit = (e, data) => {
+  const { handleNotification, trackID } = useStateContext();
+  const [order, setOrder] = useState({ packID: null, shippingID: null });
+  const submit = (e, data, hasPacks) => {
     e.preventDefault();
-
+    if (!order.packID && hasPacks) {
+      handleNotification({
+        type: "error",
+        message: "Please select pack",
+      });
+      return;
+    }
+    if (!order.shippingID) {
+      handleNotification({
+        type: "error",
+        message: "Please select your province",
+      });
+      return;
+    }
+    if (!data.fullName) {
+      handleNotification({
+        type: "error",
+        message: "Please enter your full name",
+      });
+      return;
+    }
+    if (!data.address) {
+      handleNotification({
+        type: "error",
+        message: "Please enter your address",
+      });
+      return;
+    }
+    if (!data.phoneNumber) {
+      handleNotification({
+        type: "error",
+        message: "Please enter your phone number",
+      });
+      return;
+    }
+    if (!data.postalCode) {
+      handleNotification({
+        type: "error",
+        message: "Please enter your postal code",
+      });
+      return;
+    }
     usePOST("order/create/", {
       data: data,
       headers: {
-        Authorization: token.access,
         "X-Comercify-Visitor": trackID,
       },
     }).then((res) => {
@@ -19,5 +61,5 @@ export const useCreateOrder = () => {
       if (res?.type == "error") handleNotification(res);
     });
   };
-  return { submit };
+  return { submit, setOrder, order };
 };
