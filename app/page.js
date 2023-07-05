6d9dd98a92ee3f8ee91fb905f50a8b9be7af492a
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-07-04 23:03:57 -0400

    :heavy_plus_sign: add products

--- app/page.js
+++ app/page.js
@@ -1,11 +1,23 @@
 "use client";
-import { Banner } from "@/comps";
+import { Banner, ProductCard } from "@/comps";
+import { useGetProducts } from "@/roupi/product";
 
 export default function Home() {
+  const { products } = useGetProducts({});
   return (
     <main>
       <Banner />
-      <div className="flex flex-wrap gap-4"></div>
+      <section className="flex flex-wrap my-24 gap-4 items-center justify-center">
+        {products?.map((product) => (
+          <ProductCard
+            id={product.id}
+            image={product.image}
+            title={product.title}
+            price={product.price}
+            discount={product.discount_value}
+          />
+        ))}
+      </section>
     </main>
   );
 }
